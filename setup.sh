21e94ebc5eb10588819e36e86e7758c39eba7912
Tom Daniels <trd6577@g.rit.edu>
2018-04-01 17:50:33 -0400

    Fixed output and added auto accept to apt-get

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ -d ~/.vim/bundle ]; then
     rm -rf ~/.vim/bundle
 fi
 
-sudo apt-get install git build-essential cmake python-dev python3-dev clang python-pip
+sudo apt-get install git build-essential cmake python-dev python3-dev clang python-pip -y
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/trdan6577/myvimrc.git
@@ -16,7 +16,8 @@ git submodule update --init --recursive
 ./install.py --clang-completer
 pip install flake8
 
-echo "Finished automated installation. To fully complete the installation, set"
-echo "the compiler flags in ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py"
-echo "and change line 73 in ~/.vim/bundle/vim-flake8/autoload/flake8.vim to"
-echo "call s:DeclareOption\(\'flake8_cmd\', \'\', \'\"~/.local/bin/flake8\"\'\)"
+echo  -e "\033[0;32m[+] Finished automated installation.\033[0m To fully"\
+    "complete the installation, set the compiler flags in"\
+    "~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py"\
+    "and change line 73 in ~/.vim/bundle/vim-flake8/autoload/flake8.vim to"\
+    "call s:DeclareOption\(\'flake8_cmd\', \'\', \'\"~/.local/bin/flake8\"\'\)"
